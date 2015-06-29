23e4791bfb8f55558952e281cf6ab5c3d9a5434a
markitus18 <marc18.garrigo@gmail.com>
2015-06-29 02:10:47 +0200

    Movement set

--- Unity Project/Assets/Scripts/New Scene/Box_N.cs
+++ Unity Project/Assets/Scripts/New Scene/Box_N.cs
@@ -3,13 +3,70 @@ using System.Collections;
 
 public class Box_N : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	//Linked boxes
+	public GameObject upBox;
+	public GameObject downBox;
+	public GameObject rightBox;
+	public GameObject leftBox;
+
+	Color originalColor;
+
+	void Start ()
+	{
+		//Assigning linked boxes
+		Debug.Log("Assigning linked boxes");
+		RaycastHit hit;
+		Ray upRay = new Ray(transform.position, Vector3.forward);
+		Ray downRay = new Ray(transform.position, Vector3.back);
+		Ray rightRay = new Ray(transform.position, Vector3.right);
+		Ray leftRay = new Ray(transform.position, Vector3.left);
+
+		if (Physics.Raycast(upRay, out hit, 1) && upBox == null)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				upBox = hit.transform.gameObject;
+			}
+		}
+		if (Physics.Raycast(downRay, out hit, 1) && downBox == null)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				downBox = hit.transform.gameObject;
+			}
+		}
+		if (Physics.Raycast(rightRay, out hit, 1) && rightBox == null)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				rightBox = hit.transform.gameObject;
+			}
+		}
+		if (Physics.Raycast(leftRay, out hit, 1) && leftBox == null)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				leftBox = hit.transform.gameObject;
+			}
+		}
+
+		//Saving original color
+		originalColor = transform.GetComponent<Renderer>().material.color;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+
+	void Update ()
+	{
 	
+	}
+
+	void OnMouseEnter()
+	{
+		transform.GetComponent<Renderer>().material.color = Color.blue;
+	}
+	
+	void OnMouseExit()
+	{
+		transform.GetComponent<Renderer>().material.color = originalColor;
 	}
 }
---
+++ Unity Project/Assets/Scripts/New Scene/Entity.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+
+	public GameObject currentBox;
+	public GameObject targetBox;
+
+	public float movementSpeed = 2;
+
+	Vector3 distanceToMove;
+	bool moving;
+	Vector3 currentPosition;
+	Vector3 targetPosition;
+
+	void Start ()
+	{
+		moving = false;
+		currentPosition = currentBox.transform.position;
+		transform.position = currentPosition;
+	}
+
+	void Update ()
+	{
+		if (moving)
+			Move ();
+	}
+
+	void Move()
+	{
+		if (currentPosition != targetBox.transform.position)
+		{
+			Debug.Log ("Moving");
+			distanceToMove = targetBox.transform.position - currentBox.transform.position;
+			currentPosition += distanceToMove / (10 / movementSpeed);
+			transform.position = currentPosition;
+		}
+		else
+		{
+			currentBox = targetBox;
+			moving = false;
+		}
+	}
+
+	void OnMouseUp()
+	{
+		moving = true;
+	}
+}
