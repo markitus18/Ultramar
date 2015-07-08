7d26c9b90810b99e9f3d512879fb4d9cd6c5b884
markitus18 <marc18.garrigo@gmail.com>
2015-07-08 11:00:32 +0200

    Starting dynamic environment

---
+++ Unity Project/Assets/Scripts/Standard scene/DestructiveWall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructiveWall : MonoBehaviour {
+
+	public GameObject box1;
+	public GameObject box2;
+
+	public int direction;
+
+	void Start ()
+	{
+		RaycastHit hit;
+		Ray upRay = new Ray(transform.position, Vector3.forward);
+		Ray downRay = new Ray(transform.position, Vector3.back);
+		Ray rightRay = new Ray(transform.position, Vector3.right);
+		Ray leftRay = new Ray(transform.position, Vector3.left);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
