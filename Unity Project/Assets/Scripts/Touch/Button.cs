6940b4e7fb8f30a85ed6ede829bc332811d2bfc5
Vulpem <davidher96@gmail.com>
2015-07-08 11:21:14 +0200

    Touching things!

---
+++ Unity Project/Assets/Scripts/Touch/Button.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+
+	public Color defaultColor;
+	public Color selectedColor;
+	private Material mat;
+
+	void Start ()
+	{
+
+	}
+
+	void OnTouchDown ()
+	{
+
+	}
+	void OnTouchUp ()
+	{
+		
+	}
+	void OnTouchStay ()
+	{
+		
+	}
+	void OnTouchExit ()
+	{
+		
+	}
+
+}
