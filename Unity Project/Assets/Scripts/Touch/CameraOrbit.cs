7130498f4f5634dd29103d3b7c632658b67f559c
Vulpem <davidher96@gmail.com>
2015-07-09 16:22:29 +0200

    Main camera is now even better! wiiii!

--- Unity Project/Assets/Scripts/Touch/CameraOrbit.cs
+++ Unity Project/Assets/Scripts/Touch/CameraOrbit.cs
@@ -6,15 +6,15 @@ public class CameraOrbit : MonoBehaviour
 
 		//camera variables
 	public Transform cameraLookAtTarget = null;
-	public float height = 0.0f;
-	public float rotationDamping = 3.0f;
-	public float heightDamping = 2.0f;
-	public float distance = 2.0f;
-	public float yMinLimit = -20;
-	public float yMaxLimit = 80;
-	public float xMinLimit = -100;
-	public float xMaxLimit = 100;
-	public float xSpeed = 250;
+	[Range(0,80)] public float height = 0.0f;
+	public float rotationDamping = 1.0f;
+	public float heightDamping = 0.5f;
+	[Range(0,150)] public float distance = 30.0f;
+	[Range(0,360)]public float startingY = 90;
+	[Range(0,180)]public float yMovementLimit = 45;
+	[Range(0,80)]public float xMinLimit = 45;
+	[Range(0, 80)]public float xMaxLimit = 80;
+	public float xSpeed = 75;
 	public float ySpeed = 120;
 	private float x = 0.0f;
 	private float y = 0.0f;
@@ -44,7 +44,7 @@ public class CameraOrbit : MonoBehaviour
 		if (Input.GetMouseButton (1))
 		{
 			x = ClampAngle (x - (Input.GetAxis ("Mouse Y") * xSpeed * 0.02f), xMinLimit, xMaxLimit);
-			y = ClampAngle (y + (Input.GetAxis ("Mouse X") * ySpeed * 0.02f), yMinLimit, yMaxLimit);
+			y = ClampAngle (y + (Input.GetAxis ("Mouse X") * ySpeed * 0.02f), startingY - yMovementLimit, startingY + yMovementLimit);
 			
 			transform.eulerAngles = new Vector3 (x, y, 0.0f);
 			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
@@ -53,7 +53,7 @@ public class CameraOrbit : MonoBehaviour
 		{
 			var rotationT = rotationDamping * Time.deltaTime;
 			var currentXRotation = Mathf.LerpAngle (transform.eulerAngles.x, height, rotationT);
-			var currentYRotation = Mathf.LerpAngle (transform.eulerAngles.y, 90 /*wantedRotationAngle*/, rotationT);
+			var currentYRotation = Mathf.LerpAngle (transform.eulerAngles.y, startingY, rotationT);
 			
 			transform.eulerAngles = new Vector3 (x = currentXRotation, y = currentYRotation, 0.0f);
 			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
@@ -61,50 +61,5 @@ public class CameraOrbit : MonoBehaviour
 			transform.LookAt (cameraLookAtTarget);
 		}
 	}
-
-		/*void Start()
-		{
-			var angles = gameObject.transform.eulerAngles;
-			x = angles.x;
-			y = angles.y;
-		}
-		float ClampAngle(float angle, float min, float max)
-		{
-			if (angle < -360)
-				angle += 360;
-			if (angle > 360)
-				angle -= 360;
-			return Mathf.Clamp(angle, min, max);
-		}
-
-		void LateUpdate()
-		{
-		if (cameraLookAtTarget == null) {
-			return;
-		}
-			
-		if (Input.GetMouseButton (1)) {
-			x = ClampAngle (x - (Input.GetAxis ("Mouse Y") * xSpeed * 0.02f), xMinLimit, xMaxLimit);
-			y = ClampAngle (y + (Input.GetAxis ("Mouse X") * ySpeed * 0.02f), yMinLimit, yMaxLimit);
-			distance = Mathf.Clamp (distance - (Input.GetAxis ("Mouse ScrollWheel") * 5), distanceMin, distanceMax);
-				
-			transform.eulerAngles = new Vector3 (x, y, 0.0f);
-			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
-		} else {
-			var wantedRotationAngle = cameraLookAtTarget.eulerAngles.y;
-			var wantedHeight = cameraLookAtTarget.position.y + height;
-				
-			var rotationT = rotationDamping * Time.deltaTime;
-			var currentXRotation = Mathf.LerpAngle (transform.eulerAngles.x, 0.0f, rotationT);
-			var currentYRotation = Mathf.LerpAngle (transform.eulerAngles.y, wantedRotationAngle, rotationT);
-			var currentHeight = Mathf.Lerp (transform.position.y, wantedHeight, heightDamping * Time.deltaTime);
-				
-			transform.eulerAngles = new Vector3 (x = currentXRotation, y = currentYRotation, 0.0f);
-			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
-			transform.position.Set (transform.position.x, currentHeight, transform.position.z);
-				
-			transform.LookAt (cameraLookAtTarget);
-		}
-	}*/
 
 } //End class
