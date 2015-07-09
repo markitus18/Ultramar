c294d7b7fb5eb249becdad52e21fb083de436321
Vulpem <davidher96@gmail.com>
2015-07-09 14:07:16 +0200

    Almost got it... :skull:

--- Unity Project/Assets/Scripts/Touch/CameraOrbit.cs
+++ Unity Project/Assets/Scripts/Touch/CameraOrbit.cs
@@ -5,30 +5,62 @@ public class CameraOrbit : MonoBehaviour
 {
 
 		//camera variables
-		public Transform cameraLookAtTaret = null;
-		private int mouseDown;
-		private GameObject MiTarget = null;
-		private float height = 0.0f;
-		private float rotationDamping = 3.0f;
-		private float heightDamping = 2.0f;
-		private float distance = 2.0f;
-		private float yMinLimit = -20;
-		private float yMaxLimit = 80;
-		private float xMinLimit = -100;
-		private float xMaxLimit = 100;
-		private float xSpeed = 250;
-		private float ySpeed = 120;
-		private float x = 0.0f;
-		private float y = 0.0f;
-		private float distanceMin = 1f;
-		private float distanceMax = 4f;
+	public Transform cameraLookAtTarget = null;
+	public float height = 0.0f;
+	public float rotationDamping = 3.0f;
+	public float heightDamping = 2.0f;
+	public float distance = 2.0f;
+	public float yMinLimit = -20;
+	public float yMaxLimit = 80;
+	public float xMinLimit = -100;
+	public float xMaxLimit = 100;
+	public float xSpeed = 250;
+	public float ySpeed = 120;
+	private float x = 0.0f;
+	private float y = 0.0f;
+
+	void Start()
+	{
+		var angles = gameObject.transform.eulerAngles;
+		x = angles.x;
+		y = angles.y;
+	}
+	float ClampAngle(float angle, float min, float max)
+	{
+		if (angle < -360)
+			angle += 360;
+		if (angle > 360)
+			angle -= 360;
+		return Mathf.Clamp(angle, min, max);
+	}
+	
+	void LateUpdate()
+	{
+		if (cameraLookAtTarget == null) {
+			return;
+		}
 		
-		/// <summary>
-		/// Here is where all the mouse input takes place, if you hold now mouse button down it will just follow your character and zoom in and out
-		/// if you hold down the right mouse button you can rotate around your player and zoom in and out
-		/// if you hold both left and right mouse button it will also turn your player
-		/// </summary>
-		void Start()
+		if (Input.GetMouseButton (1)) {
+			x = ClampAngle (x - (Input.GetAxis ("Mouse Y") * xSpeed * 0.02f), xMinLimit, xMaxLimit);
+			y = ClampAngle (y + (Input.GetAxis ("Mouse X") * ySpeed * 0.02f), yMinLimit, yMaxLimit);
+			
+			transform.eulerAngles = new Vector3 (x, y, 0.0f);
+			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
+		} else {
+			var wantedRotationAngle = 0;//cameraLookAtTarget.eulerAngles.y;
+			
+			var rotationT = rotationDamping * Time.deltaTime;
+			var currentXRotation = Mathf.LerpAngle (transform.eulerAngles.x, height, rotationT);
+			var currentYRotation = Mathf.LerpAngle (transform.eulerAngles.y, wantedRotationAngle, rotationT);
+			
+			transform.eulerAngles = new Vector3 (x = currentXRotation, y = currentYRotation, 0.0f);
+			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
+			
+			transform.LookAt (cameraLookAtTarget);
+		}
+	}
+
+		/*void Start()
 		{
 			var angles = gameObject.transform.eulerAngles;
 			x = angles.x;
@@ -42,9 +74,10 @@ public class CameraOrbit : MonoBehaviour
 				angle -= 360;
 			return Mathf.Clamp(angle, min, max);
 		}
+
 		void LateUpdate()
 		{
-		if (cameraLookAtTaret == null) {
+		if (cameraLookAtTarget == null) {
 			return;
 		}
 			
@@ -54,10 +87,10 @@ public class CameraOrbit : MonoBehaviour
 			distance = Mathf.Clamp (distance - (Input.GetAxis ("Mouse ScrollWheel") * 5), distanceMin, distanceMax);
 				
 			transform.eulerAngles = new Vector3 (x, y, 0.0f);
-			transform.position = cameraLookAtTaret.position - (transform.forward * distance);
+			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
 		} else {
-			var wantedRotationAngle = cameraLookAtTaret.eulerAngles.y;
-			var wantedHeight = cameraLookAtTaret.position.y + height;
+			var wantedRotationAngle = cameraLookAtTarget.eulerAngles.y;
+			var wantedHeight = cameraLookAtTarget.position.y + height;
 				
 			var rotationT = rotationDamping * Time.deltaTime;
 			var currentXRotation = Mathf.LerpAngle (transform.eulerAngles.x, 0.0f, rotationT);
@@ -65,11 +98,11 @@ public class CameraOrbit : MonoBehaviour
 			var currentHeight = Mathf.Lerp (transform.position.y, wantedHeight, heightDamping * Time.deltaTime);
 				
 			transform.eulerAngles = new Vector3 (x = currentXRotation, y = currentYRotation, 0.0f);
-			transform.position = cameraLookAtTaret.position - (transform.forward * distance);
+			transform.position = cameraLookAtTarget.position - (transform.forward * distance);
 			transform.position.Set (transform.position.x, currentHeight, transform.position.z);
 				
-			transform.LookAt (cameraLookAtTaret);
+			transform.LookAt (cameraLookAtTarget);
 		}
-	}
+	}*/
 
 } //End class
