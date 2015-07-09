33ddb9ced29c03f37aaec833112f3ca27306c26c
Vulpem <davidher96@gmail.com>
2015-07-09 13:20:53 +0200

    Still orbiting

--- Unity Project/Assets/Scripts/Touch/CameraOrbit.cs
+++ Unity Project/Assets/Scripts/Touch/CameraOrbit.cs
@@ -1,144 +1,75 @@
 using UnityEngine;
 using System.Collections;
 
-/**
- * Change the camera into an orbital camera. An orbital is a camera
- * that can be rotated and that will automatically reorient itself to
- * always point to the target.
- * 
- * The orbit camera allow zooming and dezooming with the mouse wheel.
- * 
- * By clicking the mouse and dragging on the screen, the camera is moved. 
- * The angle of rotation  correspond to the distance the cursor travelled. 
- *  
- * The camera will keep the angular position when the button is pressed. To
- * rotate more, simply repress the mouse button et move the cursor.
- *
- * This script must be added on a camera object.
- *
- * @author Mentalogicus
- * @date 11-2011
- */
 public class CameraOrbit : MonoBehaviour
 {
-	
-	//The target of the camera. The camera will always point to this object.
-	public Transform _target;
-	
-	//The default distance of the camera from the target.
-	public float _distance = 20.0f;
-	
-	//Control the speed of zooming and dezooming.
-	public float _zoomStep = 1.0f;
-	
-	//The speed of the camera. Control how fast the camera will rotate.
-	public float _xSpeed = 1f;
-	public float _ySpeed = 1f;
-	
-	//The position of the cursor on the screen. Used to rotate the camera.
-	private float _x = 0.0f;
-	private float _y = 0.0f;
-	
-	//Distance vector. 
-	private Vector3 _distanceVector;
-	
-	/**
-  * Move the camera to its initial position.
-  */
-	void Start ()
-	{
-		_distanceVector = new Vector3(0.0f,0.0f,-_distance);
+
+		//camera variables
+		public Transform cameraLookAtTaret = null;
+		private int mouseDown;
+		private GameObject MiTarget = null;
+		private float height = 0.0f;
+		private float rotationDamping = 3.0f;
+		private float heightDamping = 2.0f;
+		private float distance = 2.0f;
+		private float yMinLimit = -20;
+		private float yMaxLimit = 80;
+		private float xMinLimit = -100;
+		private float xMaxLimit = 100;
+		private float xSpeed = 250;
+		private float ySpeed = 120;
+		private float x = 0.0f;
+		private float y = 0.0f;
+		private float distanceMin = 1f;
+		private float distanceMax = 4f;
 		
-		Vector2 angles = this.transform.localEulerAngles;
-		_x = angles.x;
-		_y = angles.y;
-		
-		this.Rotate(_x, _y);
-		
-	}
-	
-	/**
-  * Rotate the camera or zoom depending on the input of the player.
-  */
-	void LateUpdate()
-	{
-		if ( _target )
+		/// <summary>
+		/// Here is where all the mouse input takes place, if you hold now mouse button down it will just follow your character and zoom in and out
+		/// if you hold down the right mouse button you can rotate around your player and zoom in and out
+		/// if you hold both left and right mouse button it will also turn your player
+		/// </summary>
+		void Start()
 		{
-			this.RotateControls();
-			this.Zoom();
+			var angles = gameObject.transform.eulerAngles;
+			x = angles.x;
+			y = angles.y;
 		}
-	}
-	
-	/**
-  * Rotate the camera when the first button of the mouse is pressed.
-  * 
-  */
-	void RotateControls()
-	{
-		if ( Input.GetButton("Fire1") )
+		float ClampAngle(float angle, float min, float max)
 		{
-			_x += Input.GetAxis("Mouse X") * _xSpeed;
-			_y += -Input.GetAxis("Mouse Y")* _ySpeed;
+			if (angle < -360)
+				angle += 360;
+			if (angle > 360)
+				angle -= 360;
+			return Mathf.Clamp(angle, min, max);
+		}
+		void LateUpdate()
+		{
+		if (cameraLookAtTaret == null) {
+			return;
+		}
 			
-			this.Rotate(_x,_y);
+		if (Input.GetMouseButton (1)) {
+			x = ClampAngle (x - (Input.GetAxis ("Mouse Y") * xSpeed * 0.02f), xMinLimit, xMaxLimit);
+			y = ClampAngle (y + (Input.GetAxis ("Mouse X") * ySpeed * 0.02f), yMinLimit, yMaxLimit);
+			distance = Mathf.Clamp (distance - (Input.GetAxis ("Mouse ScrollWheel") * 5), distanceMin, distanceMax);
+				
+			transform.eulerAngles = new Vector3 (x, y, 0.0f);
+			transform.position = cameraLookAtTaret.position - (transform.forward * distance);
+		} else {
+			var wantedRotationAngle = cameraLookAtTaret.eulerAngles.y;
+			var wantedHeight = cameraLookAtTaret.position.y + height;
+				
+			var rotationT = rotationDamping * Time.deltaTime;
+			var currentXRotation = Mathf.LerpAngle (transform.eulerAngles.x, 0.0f, rotationT);
+			var currentYRotation = Mathf.LerpAngle (transform.eulerAngles.y, wantedRotationAngle, rotationT);
+			var currentHeight = Mathf.Lerp (transform.position.y, wantedHeight, heightDamping * Time.deltaTime);
+				
+			transform.eulerAngles = new Vector3 (x = currentXRotation, y = currentYRotation, 0.0f);
+			transform.position = cameraLookAtTaret.position - (transform.forward * distance);
+			transform.position.Set (transform.position.x, currentHeight, transform.position.z);
+				
+			transform.LookAt (cameraLookAtTaret);
 		}
-		
 	}
-	
-	/**
-  * Transform the cursor mouvement in rotation and in a new position
-  * for the camera.
-  */
-	void Rotate( float x, float y )
-	{
-		//Transform angle in degree in quaternion form used by Unity for rotation.
-		Quaternion rotation = Quaternion.Euler(y,x,0.0f);
-		
-		//The new position is the target position + the distance vector of the camera
-		//rotated at the specified angle.
-		Vector3 position = rotation * _distanceVector + _target.position;
-		
-		//Update the rotation and position of the camera.
-		transform.rotation = rotation;
-		transform.position = position;
-	}
-	
-	/**
-  * Zoom or dezoom depending on the input of the mouse wheel.
-  */
-	void Zoom()
-	{
-		if ( Input.GetAxis("Mouse ScrollWheel") < 0.0f )
-		{
-			this.ZoomOut();
-		}
-		else if ( Input.GetAxis("Mouse ScrollWheel") > 0.0f )
-		{
-			this.ZoomIn();
-		}
-		
-	}
-	
-	/**
-  * Reduce the distance from the camera to the target and
-  * update the position of the camera (with the Rotate function).
-  */
-	void ZoomIn()
-	{
-		_distance -= _zoomStep;
-		_distanceVector = new Vector3(0.0f,0.0f,-_distance);
-		this.Rotate(_x,_y);
-	}
-	
-	/**
-  * Increase the distance from the camera to the target and
-  * update the position of the camera (with the Rotate function).
-  */
-	void ZoomOut()
-	{
-		_distance += _zoomStep;
-		_distanceVector = new Vector3(0.0f,0.0f,-_distance);
-		this.Rotate(_x,_y);
-	}
-	
+
 } //End class
