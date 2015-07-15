76c38840e98a3399a8eb75e4cea73942e253591a
markitus18 <marc18.garrigo@gmail.com>
2015-07-15 09:41:59 +0200

    Starting level selection

---
+++ Unity Project/Assets/Scripts/Level Selection/Box_L.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Box_L : MonoBehaviour
+{
+	//Directions in which you can move
+	public bool up;
+	public bool right;
+	public bool down;
+	public bool left;
+	//Linked boxes
+	[HideInInspector] public GameObject upBox;
+	[HideInInspector] public GameObject downBox;
+	[HideInInspector] public GameObject rightBox;
+	[HideInInspector] public GameObject leftBox;
+
+	public int boxDistance;
+	public float entityDistanceX;
+	public float entityDistanceZ;
+//	public Material standardMaterial;
+	PlayerController playerController;
+	/*[HideInInspector]*/ public List<GameObject> enemies;
+
+	public Material standardMaterial;
+	[HideInInspector] public Material originalMaterial;
+	public Material highlightMaterial;
+
+	void Awake ()
+	{
+		//Assigning linked boxes
+		RaycastHit hit;
+		Ray upRay = new Ray(transform.position, Vector3.forward);
+		Ray downRay = new Ray(transform.position, Vector3.back);
+		Ray rightRay = new Ray(transform.position, Vector3.right);
+		Ray leftRay = new Ray(transform.position, Vector3.left);
+
+		if (Physics.Raycast(upRay, out hit, boxDistance) && upBox == null && up==true)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				upBox = hit.transform.gameObject;
+			}
+		}
+		if (Physics.Raycast(downRay, out hit, boxDistance) && downBox == null && up==down)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				downBox = hit.transform.gameObject;
+			}
+		}
+		if (Physics.Raycast(rightRay, out hit, boxDistance) && rightBox == null && right==true)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				rightBox = hit.transform.gameObject;
+			}
+		}
+		if (Physics.Raycast(leftRay, out hit, boxDistance) && leftBox == null && left==true)
+		{
+			if(hit.collider.tag == "Box")
+			{
+				leftBox = hit.transform.gameObject;
+			}
+		}
+
+		//Setting all variables
+		playerController = GameObject.FindWithTag ("Player").GetComponent<PlayerController>();
+		//Saving original color
+		originalMaterial = transform.GetComponent<Renderer>().material;
+	}
+
+	void OnTouchStay()
+	{
+		transform.GetComponent<Renderer>().material = highlightMaterial;
+	}
+	
+	void OnTouchExit()
+	{
+		transform.GetComponent<Renderer>().material = originalMaterial;
+	}
+
+	void OnTouchUp()
+	{
+		Debug.Log ("doing something");
+		transform.GetComponent<Renderer>().material = originalMaterial;
+		playerController.SetNewBox(gameObject);
+	}
+
+	public void UpdatePosition(GameObject enemy)
+	{
+		int groups = Mathf.CeilToInt((float)enemies.Count / 2);
+		int sign;
+		if (groups % 2 == 1)
+		{
+			for (int i = 0; i < groups; i++)
+			{
+				sign = (i % 2 == 0) ?  -1 : 1;
+
+ 				if ((i * 2) + 1 < enemies.Count)
+				{
+					enemies[i * 2].GetComponent<Entity>().targetPosition.z = gameObject.transform.position.z + entityDistanceZ;
+					enemies[(i * 2)+ 1].GetComponent<Entity>().targetPosition.z = gameObject.transform.position.z - entityDistanceZ;
+
+					enemies[i * 2].GetComponent<Entity>().targetPosition.x = gameObject.transform.position.x + (entityDistanceX * Mathf.CeilToInt((float)i / 2) * sign);
+					enemies[(i * 2)+ 1].GetComponent<Entity>().targetPosition.x = gameObject.transform.position.x + (entityDistanceX * Mathf.CeilToInt((float)i / 2) * sign);
+				}
+				else
+				{
+					enemies[i * 2].GetComponent<Entity>().targetPosition.z = gameObject.transform.position.z;
+
+
+					enemies[i * 2].GetComponent<Entity>().targetPosition.x = gameObject.transform.position.x + (entityDistanceX * Mathf.CeilToInt((float)i / 2) * sign);
+
+				}
+			}
+		}
+		else if (groups % 2 == 0)
+		{
+			for (int i = 0; i < groups; i++)
+			{
+				sign = (i % 2 == 0) ?  -1 : 1;
+				if ((i * 2) + 1 < enemies.Count)
+				{
+					enemies[i * 2].GetComponent<Entity>().targetPosition.z = gameObject.transform.position.z + entityDistanceZ;
+					enemies[(i * 2)+ 1].GetComponent<Entity>().targetPosition.z = gameObject.transform.position.z - entityDistanceZ;
+
+					enemies[i * 2].GetComponent<Entity>().targetPosition.x = gameObject.transform.position.x + ((entityDistanceX * 0.5f + entityDistanceX * Mathf.FloorToInt((float)i / 2)) * sign);
+					enemies[(i * 2)+ 1].GetComponent<Entity>().targetPosition.x = gameObject.transform.position.x + ((entityDistanceX * 0.5f + entityDistanceX * Mathf.FloorToInt((float)i / 2)) * sign);
+				}
+				else
+				{
+					enemies[i * 2].GetComponent<Entity>().targetPosition.z = gameObject.transform.position.z;
+					enemies[i * 2].GetComponent<Entity>().targetPosition.x = gameObject.transform.position.x + ((entityDistanceX * 0.5f + entityDistanceX * Mathf.FloorToInt((float)i / 2)) * sign);
+				}
+			}
+		}
+		
+		if (enemy.GetComponent<Entity>().targetPosition != enemy.GetComponent<Entity>().currentPosition)
+		{
+			enemy.GetComponent<Entity>().distanceToMove = enemy.GetComponent<Entity>().targetPosition - enemy.transform.position;
+			enemy.GetComponent<Entity>().moving = true;
+		}
+	}
+}
---
+++ Unity Project/Assets/Scripts/Level Selection/GameStateMachine_L.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameStateMachine_L : MonoBehaviour
+{
+	public enum GameStates_L
+	{
+		PLAYER_TURN,
+		ENEMY_START,
+		ENEMY_MOVE,
+		ENEMY_END,
+		END,
+	}
+	
+	public enum UpdateStates_L
+	{
+		UPDATE_KEEP,
+		UPDATE_NEXT,
+	}
+	
+	public Entity playerController;
+	public List<GameObject> enemies;
+	GameObject[] go;
+	public GameStates_L state;
+
+	void Start ()
+	{
+		playerController = GameObject.FindWithTag("Player").GetComponent<Entity>();
+		state = GameStates_L.PLAYER_TURN;
+		go = GameObject.FindGameObjectsWithTag("Enemy");
+		foreach (GameObject enemy in go)
+		{
+			enemies.Add(enemy);
+		}
+		
+	}
+
+	void Update ()
+	{
+		switch(state)
+		{
+		case GameStates_L.PLAYER_TURN:
+			UpdatePlayer();
+			break;
+		case GameStates_L.ENEMY_START:
+			StartEnemiesTurn ();
+			break;
+		default:
+			break;
+		}
+	}
+
+	void UpdatePlayer()
+	{
+		if (playerController.gameObject.GetComponent<PlayerController>().autoPassTurn)
+		{
+			state = GameStates_L.ENEMY_START;
+		}
+		else if (!playerController.gameObject.GetComponent<PlayerController>().paused)
+		{
+			if (playerController.UpdateEntity() == GameStateMachine.UpdateStates.UPDATEEXT)
+			{
+				state = GameStates_L.ENEMY_START;
+				playerController.gameObject.GetComponent<PlayerController>().CheckEnemy();
+				if (playerController.gameObject.GetComponent<PlayerController>().CheckEnd())
+					state = GameStates_L.END;
+				Debug.Log("Enemies turn");
+			}
+		}
+	}
+
+	void StartEnemiesTurn()
+	{
+		state = GameStates_L.PLAYER_TURN;
+	}
+	/*
+	void ResetEnemiesTurn()
+	{
+		int enemiesMax = enemies.Count;
+		for (int i = 0; i < enemiesMax; i++)
+		{
+			enemies[i].GetComponent<Entity>().ret = UpdateStates.UPDATE_KEEP;
+			enemies[i].GetComponent<Entity>().targetAssigned = false;
+		}
+	}
+
+	void SetEnemiesBox()
+	{
+		Debug.Log("Setting enemies boxes");
+		int enemiesMax = gameObject.GetComponent<GameStateMachine_L_L>().enemies.Count;
+		for (int i = 0; i < enemiesMax; i++)
+		{
+			if (enemies[i].GetComponent<StaticEnemyC>())
+				enemies[i].GetComponent<StaticEnemyC>().SetNewBox();
+			if (enemies[i].GetComponent<RunnerEnemyC>() || enemies[i].GetComponent<CavalryEnemyC>())
+				enemies[i].GetComponent<Entity>().SetNewBox();	
+		}
+	}
+
+	void SetEnemiesPositions()
+	{
+		int enemiesMax = gameObject.GetComponent<GameStateMachine_L>().enemies.Count;
+		for (int i = 0; i < enemiesMax; i++)
+		{
+			enemies[i].GetComponent<Entity>().SetNewPosition ();
+		}
+	}
+
+	void MoveEnemies()
+	{
+		int enemiesMax = enemies.Count;
+		int enemiesUpdated = 0;
+		for (int i = 0; i < enemiesMax; i++)
+		{
+			if (enemies[i].GetComponent<Entity>().ret == UpdateStates.UPDATE_KEEP)
+			{
+				enemies[i].GetComponent<Entity>().Move();
+			}
+			else
+				enemiesUpdated ++;
+
+		}
+		if (enemiesUpdated == enemiesMax)
+		{
+			if (CheckPlayerKill ())
+				state = GameStates.END;
+			else
+				state = GameStates.ENEMY_END;
+		}
+	}
+
+	bool CheckPlayerKill()
+	{
+		bool ret = false;
+		int enemiesMax = gameObject.GetComponent<GameStateMachine_L>().enemies.Count;
+		for (int i = 0; i < enemiesMax; i++)
+		{
+			if (enemies[i].GetComponent<StaticEnemyC>())
+				if (enemies[i].GetComponent<StaticEnemyC>().CheckIfPlayer()) ret = true;
+			if (enemies[i].GetComponent<RunnerEnemyC>())
+				if (enemies[i].GetComponent<RunnerEnemyC>().CheckIfPlayer ()) ret = true;
+			if (enemies[i].GetComponent<ArcherEnemyC>())
+				if (enemies[i].GetComponent<ArcherEnemyC>().CheckPlayer()) ret = true;
+			if (enemies[i].GetComponent<CavalryEnemyC>())
+				if (enemies[i].GetComponent<CavalryEnemyC>().CheckIfPlayer()) ret = true;
+		}
+		return ret;
+	}
+	void EndEnemiesTurn()
+	{
+
+		SetDirection();
+		state = GameStates.PLAYER_TURN;
+	}
+	
+	void SetDirection()
+	{
+		int enemiesMax = gameObject.GetComponent<GameStateMachine_L>().enemies.Count;
+		for (int i = 0; i < enemiesMax; i ++)
+		{
+			if (enemies[i].GetComponent<RunnerEnemyC>())
+				enemies[i].GetComponent<RunnerEnemyC>().SetNewDirection();
+			if (enemies[i].GetComponent<CavalryEnemyC>())
+				enemies[i].GetComponent<CavalryEnemyC>().SetNewDirection();
+		}
+	}
+	*/
+}
