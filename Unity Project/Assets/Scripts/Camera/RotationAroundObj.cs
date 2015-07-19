51919ef09edd1393c5ecc86c6f94fe9e559760f0
markitus18 <marc18.garrigo@gmail.com>
2015-07-20 01:32:38 +0200

    Starting intro scene

---
+++ Unity Project/Assets/Scripts/Camera/RotationAroundObj.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationAroundObj : MonoBehaviour
+{
+	public float speed = 1;
+	public Transform target;
+	
+	void Update()
+	{
+		transform.LookAt(target);
+		transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+	}
+}
