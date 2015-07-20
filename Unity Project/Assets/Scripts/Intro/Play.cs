6dc0a413bec68322e7ba76d9e3c9e4fac3911596
markitus18 <marc18.garrigo@gmail.com>
2015-07-20 13:12:18 +0200

    OnTouchUp wont work :/

---
+++ Unity Project/Assets/Scripts/Intro/Play.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour
+{
+	public int levelToLoad;
+	
+	void OnTouchUp()
+	{
+		Application.LoadLevel (levelToLoad);
+	}
+}
