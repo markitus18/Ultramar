6a08bd700d5366e1df24a6a4ba5a74423ca508fd
markitus18 <marc18.garrigo@gmail.com>
2015-07-20 16:38:06 +0200

    Playing with some stuff

---
+++ Unity Project/Assets/Scripts/Intro/GUIController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour
+{
+
+	public float hSliderValue = 10.0F;
+
+	void OnGUI()
+	{
+		hSliderValue = GUI.HorizontalSlider(new Rect(200, 25, 100, 30), hSliderValue, 0.0F, 10.0F);
+	}
+
+	void Update()
+	{
+		AudioListener.volume = hSliderValue/100.0F;
+	}
+}
