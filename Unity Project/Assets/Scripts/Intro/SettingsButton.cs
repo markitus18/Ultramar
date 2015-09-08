c34909638a419accd3a15581c51fd6f46ce74167
markitus18 <marc18.garrigo@gmail.com>
2015-09-08 13:19:25 +0200

    GUI start

---
+++ Unity Project/Assets/Scripts/Intro/SettingsButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsButton : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject panel;
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
