0f46df50be51aa3fdb240175073735ddbeb750b2
markitus18 <marc18.garrigo@gmail.com>
2015-09-08 18:25:42 +0200

    More panel shit

--- Unity Project/Assets/Scripts/Intro/PanelManager.cs
+++ Unity Project/Assets/Scripts/Intro/PanelManager.cs
@@ -2,13 +2,35 @@
 using System.Collections;
 
 public class PanelManager : MonoBehaviour {
-
+	
 	public GameObject panel;
+
+	public GameObject disabledImage;
+	public GameObject soundButton;
 
 	public void OpenPanel()
 	{
 		panel.SetActive (true);
 	}
 
+	public void ClosePanel()
+	{
+		panel.SetActive(false);
+	}
+
+	public void UpdateVolume()
+	{
+
+		if (AudioListener.volume == 1)
+		{
+			AudioListener.volume = 0;
+//			soundButton.GetComponent<Image>() = disabledImage;
+		}
+		else
+		{
+			AudioListener.volume = 1;
+	//		soundButton.GetComponent<Image>() = disabledImage;
+		}
+	}
 
 }
