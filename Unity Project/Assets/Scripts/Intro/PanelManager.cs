fdf5f7fb044c0f42a63b6c7bdb53ab2887f5fc09
markitus18 <marc18.garrigo@gmail.com>
2015-09-08 17:08:49 +0200

    Button now opens a panel :3

---
+++ Unity Project/Assets/Scripts/Intro/PanelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelManager : MonoBehaviour {
+
+	public GameObject panel;
+
+	public void OpenPanel()
+	{
+		panel.SetActive (true);
+	}
+
+
+}
