eb1ff9b2ff67e1d0a19346132d9a7346550b9e17
Vulpem <davidher96@gmail.com>
2015-09-27 22:15:55 +0200

    asdadasdas

--- Unity Project/Assets/Scripts/PlayMovie.cs
+++ Unity Project/Assets/Scripts/PlayMovie.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class PlayMovie : MonoBehaviour {
 
+    Rect ZeroPos;
+
 	// Use this for initialization
 	void Start ()
     {
         //public string  Path;
-
+#if !UNITY_EDITOR
         Handheld.PlayFullScreenMovie("Cinematica01.mp4", Color.black, FullScreenMovieControlMode.Hidden, FullScreenMovieScalingMode.AspectFit);
         Application.LoadLevel(2);
+#endif
+#if UNITY_EDITOR
+        ZeroPos.x = ZeroPos.y = 0;
+        ZeroPos.width = Screen.width;
+        ZeroPos.height = Screen.height;
+        UnityEngine.Texture
+
+        GUI.DrawTexture(ZeroPos, "Cinematica01.mp4", ScaleMode.ScaleToFit);
+
+#endif
 
     }
 	
