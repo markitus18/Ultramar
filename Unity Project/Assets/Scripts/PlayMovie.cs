51396bff250eb740c043985e7e9ad7e34376112f
Vulpem <davidher96@gmail.com>
2015-09-28 20:48:06 +0200

    Music added and cinematic on PC

--- Unity Project/Assets/Scripts/PlayMovie.cs
+++ Unity Project/Assets/Scripts/PlayMovie.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class PlayMovie : MonoBehaviour {
 
-    Rect ZeroPos;
+    public MovieTexture movie;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         //public string  Path;
 #if !UNITY_EDITOR
@@ -14,19 +14,27 @@ public class PlayMovie : MonoBehaviour {
         Application.LoadLevel(2);
 #endif
 #if UNITY_EDITOR
-       /* ZeroPos.x = ZeroPos.y = 0;
-        ZeroPos.width = Screen.width;
-        ZeroPos.height = Screen.height;
-        UnityEngine.Texture
+        /* ZeroPos.x = ZeroPos.y = 0;
+         ZeroPos.width = Screen.width;
+         ZeroPos.height = Screen.height;
+         UnityEngine.Texture
 
-        GUI.DrawTexture(ZeroPos, "Cinematica01.mp4", ScaleMode.ScaleToFit);*/
+         GUI.DrawTexture(ZeroPos, "Cinematica01.mp4", ScaleMode.ScaleToFit);*/
 
+       GetComponent<Renderer>().material.mainTexture = movie;
+        movie.Play();
+
+        
 #endif
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!movie.isPlaying)
+        {
+            Application.LoadLevel(2);
+        }
+    }
 }
