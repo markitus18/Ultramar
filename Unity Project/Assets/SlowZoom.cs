a22d5d3f90078398256e560ec9cb1533ce792f24
Vulpem <davidher96@gmail.com>
2015-09-29 21:08:45 +0200

    Minor camera zoom intro

---
+++ Unity Project/Assets/SlowZoom.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowZoom : MonoBehaviour {
+
+    // The target marker.
+    public Vector3 destination;
+	
+	public float speed = 0.5f;
+    public float maxSpeed = 0.5f;
+	
+    void Start()
+    {
+        speed = 0;
+    }
+	
+	void Update()
+    {
+        // The step size is equal to speed times frame time.
+        if (speed < maxSpeed)
+        {
+            speed += maxSpeed / 2000;
+        }
+
+
+        var step = speed * Time.deltaTime;
+
+        // Move our position a step closer to the target.
+        transform.position = Vector3.MoveTowards(transform.position, destination, step);
+    }
+}
