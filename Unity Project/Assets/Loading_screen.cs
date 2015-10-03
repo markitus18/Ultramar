0a1c9197c032a85b36a2a9ba1a2aa570df637026
Vulpem <davidher96@gmail.com>
2015-10-03 19:42:38 +0200

    Fading, wiii!

--- Unity Project/Assets/Loading_screen.cs
+++ Unity Project/Assets/Loading_screen.cs
@@ -4,34 +4,35 @@ using System.Collections;
 public class Loading_screen : MonoBehaviour {
 
     public Texture image;
+    Color GuiColor;
 
     // Use this for initialization
     void Start () {
-	
+        GuiColor.a = 1.0f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-    }
 
-    
-    void OnGUI()
+    // Update is called once per frame
+    void Update()
     {
-        Color GuiColor;
-        GuiColor.r = GuiColor.g = GuiColor.b = 1.0f;
-        GuiColor.a = 0.5f;
-        GUI.color = GuiColor;
-
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), image);
-
-        /*if (GuiColor.a > 0)
+        if (GuiColor.a > 0)
         {
             GuiColor.a -= 0.01f;
         }
         else
         {
             GuiColor.a = 0.0f;
-        }*/
+        }
+    }
+
+
+    void OnGUI()
+    {
+
+
+
+        GuiColor.r = GuiColor.g = GuiColor.b = 1.0f;
+        GUI.color = GuiColor;
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), image);
     }
 }
