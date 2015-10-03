c6c12a5fd60b2c0ad8d44a1477031ab7c2edbbde
Vulpem <davidher96@gmail.com>
2015-10-03 09:43:43 +0200

    cinematiques mobile fixes

---
+++ Unity Project/Assets/Disable_if_not_Unity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disable_if_not_Unity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+#if !UNITY_EDITOR
+        gameObject.SetActive(false);
+#endif
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
