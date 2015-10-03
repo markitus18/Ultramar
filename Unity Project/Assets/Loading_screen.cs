994a09e2d4b193ec2bef9db261c565ed47987b70
Vulpem <davidher96@gmail.com>
2015-10-03 19:26:54 +0200

    Testing transparencu loading scren

---
+++ Unity Project/Assets/Loading_screen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loading_screen : MonoBehaviour {
+
+    public Texture image;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+    void OnGUI()
+    {
+        Color GuiColor;
+        GuiColor.r = GuiColor.g = GuiColor.b = 1.0f;
+        GuiColor.a = 0.5f;
+        GUI.color = GuiColor;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), image);
+    }
+}
