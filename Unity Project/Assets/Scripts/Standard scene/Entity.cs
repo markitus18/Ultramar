2ead316836fd81ffb052d6e44a5ca22a0b688b3f
markitus18 <marc18.garrigo@gmail.com>
2015-10-08 00:30:28 +0200

    Commit for canvas?

--- Unity Project/Assets/Scripts/Standard scene/Entity.cs
+++ Unity Project/Assets/Scripts/Standard scene/Entity.cs
@@ -10,7 +10,7 @@ public class Entity : MonoBehaviour
 	public float movementSpeed = 2;
 
 	public Vector3 distanceToMove;
-	[HideInInspector] public bool moving;
+	/*[HideInInspector] */public bool moving;
 	[HideInInspector] public bool targetAssigned;
 	[HideInInspector] public Vector3 currentPosition;
 	[HideInInspector] public Vector3 targetPosition;
